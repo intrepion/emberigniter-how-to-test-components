54d18501de29101b93d9d5435da8258d47fc840c
Oliver Forral <intrepion@gmail.com>
2015-12-04 17:42:09 -0800

    green

--- app/components/wine-stock.js
+++ app/components/wine-stock.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    submit() {
+      const wine = this.get('selectedWine');
+      const newQty = this.get('selectedQuantity');
+      wine.decrementProperty('quantity', newQty);
+      this.attrs.sellWines(wine);
+
+      // reset form
+      this.setProperties({ selectedWine: null, selectedQuantity: null });
+
+    }
+  }
+
 });
+
