9f65e2fe867a80b2d2ac9c44a4710201e3839fa6
Oliver Forral <intrepion@gmail.com>
2015-12-04 16:35:22 -0800

    red

--- tests/integration/components/wine-stock-test.js
+++ tests/integration/components/wine-stock-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import makeWineObjects from '../../helpers/make-wine-objects';
 
 moduleForComponent('wine-stock', 'Integration | Component | wine stock', {
   integration: true
@@ -13,5 +14,30 @@ test('it renders empty', function(assert) {
   // check title is correct
 
   assert.equal(this.$('h1').text(), 'Wine Stock');
+
+});
+
+test('it renders wines in dropdown and list', function(assert) {
+  assert.expect(5);
+
+  const model = makeWineObjects(12);
+
+  this.set('model', model);
+  this.render(hbs`{{wine-stock wines=model}}`);
+
+  // check wines are included in dropdown
+
+  const $dropdown = this.$('.dropdown');
+
+  assert.equal($dropdown.find('option:eq(0)').text(), model[0].toString());
+  assert.equal($dropdown.find('option:eq(1)').text(), model[1].toString());
+
+  // check all wines are listed
+
+  const $list = this.$('.wines');
+
+  assert.equal($list.find('.wine:eq(0)').text(), model[0].toString());
+  assert.equal($list.find('.wine:eq(8)').text(), model[8].toString());
+  assert.ok($list.find('.wine').length);
 
 });
